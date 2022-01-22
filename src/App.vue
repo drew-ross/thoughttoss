5a8cb8092d3328b9d84889198dfec71cba3d03d2
Drew <mail.drew.ross@gmail.com>
2022-01-21 17:44:47 -0800

    Add addBlock method, button to test adding Blocks

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,38 @@
 <template>
   <div class="Blocks">
     <Block v-for="(block, i) in blocks" :key="i" :block="block" />
+    <button v-on:click="addBlock(0)">Add Block</button>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+
 import Block from "./components/Block.vue";
+
+import suggestions from "./data/suggestions";
+import { IBlock } from "./interfaces";
 
 export default defineComponent({
   name: "App",
   components: {
     Block,
   },
-  data: () => ({
-    blocks: [1, 2, 3],
-  }),
+  data() {
+    return {
+      blocks: [] as IBlock[],
+    };
+  },
+  methods: {
+    addBlock(index: number) {
+      this.blocks.push({
+        heading: suggestions[index].text,
+        cols: suggestions[index].cols,
+        text1: "",
+        text2: "",
+      });
+    },
+  },
 });
 </script>
 
