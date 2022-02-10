2105102985b561af5d4a410b7e5495e7898b9447
Drew <mail.drew.ross@gmail.com>
2022-02-09 18:03:17 -0800

    Add example paragraphs, add .Canvas padding and p font-size

--- src/components/Canvas.vue
+++ src/components/Canvas.vue
@@ -1,5 +1,18 @@
 <template>
-  <div class="Canvas"></div>
+  <div class="Canvas">
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
+      consequuntur eos veritatis dicta autem odit aliquid cumque ad, nostrum
+      doloribus, officiis tenetur itaque quos voluptate voluptatibus beatae,
+      iste sapiente accusamus!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
+      consequuntur eos veritatis dicta autem odit aliquid cumque ad, nostrum
+      doloribus, officiis tenetur itaque quos voluptate voluptatibus beatae,
+      iste sapiente accusamus!
+    </p>
+  </div>
 </template>
 
 <script lang="ts">
@@ -13,5 +26,10 @@ export default defineComponent({
 <style scoped>
 .Canvas {
   height: 100%;
+  padding: 6rem;
+}
+
+p {
+  font-size: 1.5rem;
 }
 </style>
