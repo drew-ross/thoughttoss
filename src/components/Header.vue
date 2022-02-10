412419174d5c1db5ab8ccc71c55bd00c10cc4dae
Drew <mail.drew.ross@gmail.com>
2022-02-09 17:58:53 -0800

    Add header

---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <header class="Header">
+    <h1>thoughttoss</h1>
+  </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Header",
+});
+</script>
+
+<style scoped>
+.Header {
+  width: 100%;
+  padding: 1rem 6rem;
+  background: var(--white);
+  box-shadow: var(--smooth-shadow);
+}
+
+h1 {
+  font-size: 1.8rem;
+  color: var(--theme-primary);
+  font-family: var(--font-heading);
+}
+</style>
