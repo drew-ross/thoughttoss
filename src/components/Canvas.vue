fc36fe763b360531395e7901e410c4b3d1cdb2f1
Drew <mail.drew.ross@gmail.com>
2022-02-09 17:16:20 -0800

    Add Canvas init component

---
+++ src/components/Canvas.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="Canvas"></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Canvas",
+});
+</script>
+
+<style scoped>
+.Canvas {
+  height: 100%;
+}
+</style>
