d0b483d0eb5bf6311f2fbde455195bf4e3a4c976
Drew <mail.drew.ross@gmail.com>
2022-03-07 11:26:08 -0800

    Refactor themeColor functionality to iterate through array of colors, implement themeColor switching

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="App" :class="theme">
     <Header
-      @theme-mode="switchThemeMode"
       :theme="theme"
-      :themeColor="themeColor"
+      :themeColor="themeColor.color"
+      @theme-mode="switchThemeMode"
+      @theme-color="switchThemeColor"
     />
-    <Canvas :theme="theme" :themeColor="themeColor" />
+    <Canvas :theme="theme" :themeColor="themeColor.color" />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 
-import { Theme, ThemeColor } from "./types";
+import { Theme } from "./types";
+import { IThemeColor } from "./interfaces";
+import { THEME_COLORS } from "./constants";
 
 import Canvas from "./components/Canvas.vue";
 import Header from "./components/Header.vue";
@@ -26,7 +29,7 @@ export default defineComponent({
   data() {
     return {
       theme: localStorage.getItem("theme") || ("light" as Theme),
-      themeColor: localStorage.getItem("themeColor") || ("green" as ThemeColor),
+      themeColor: this.loadThemeColor(),
     };
   },
   methods: {
@@ -34,9 +37,34 @@ export default defineComponent({
       this.theme === "light" ? (this.theme = "dark") : (this.theme = "light");
       localStorage.setItem("theme", this.theme);
     },
-    switchThemeColor: function (color: ThemeColor) {
-      this.themeColor = color;
-      localStorage.setItem("themeColor", this.themeColor);
+    switchThemeColor: function () {
+      // get the index for the next theme color
+      let newThemeIdx = this.themeColor.index + 1;
+      if (newThemeIdx >= THEME_COLORS.length) newThemeIdx = 0;
+      // create new themeColor object
+      const newTheme = {
+        index: newThemeIdx,
+        color: THEME_COLORS[newThemeIdx],
+      } as IThemeColor;
+      // save new themeColor to state and localStorage
+      this.themeColor = newTheme;
+      this.saveThemeColor(newTheme);
+    },
+    loadThemeColor: function () {
+      let themeColor = { index: 0, color: THEME_COLORS[0] } as IThemeColor;
+      const loadedThemeColor = JSON.parse(
+        localStorage.getItem("themeColor") as string
+      );
+      if (loadedThemeColor) {
+        themeColor = loadedThemeColor;
+      } else {
+        // if there is no stored value, store default
+        this.saveThemeColor(themeColor);
+      }
+      return themeColor;
+    },
+    saveThemeColor: function (themeColor: IThemeColor) {
+      localStorage.setItem("themeColor", JSON.stringify(themeColor));
     },
   },
 });
